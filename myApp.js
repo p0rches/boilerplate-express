15a74a7b382841dc782604fcf339724d74386ae2
Alex <ascott.sco@gmail.com>
2023-09-04 19:35:19 +0100

    get user with query input

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ const path = __dirname + "/views/index.html";
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
+app.get("/", function(res) {
   res.sendFile(path);
 });
 
@@ -22,14 +22,29 @@ app.get("/", function(req, res) {
 //  res.json(obj);
 //});
 
-app.use("/", (req, res, next) => {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
+//app.use("/", (req, res, next) => {
+//  console.log(`${req.method} ${req.path} - ${req.ip}`);
+//  next();
+//});
+
+app.get("/now", (req, next) => {
+  req.time = new Date().toString();
   next();
+}, (req, res) => {
+  res.json({ time: req.time })
+});
+
+app.get("/:word/echo", (req, res) => {
+  res.json({ echo: req.params.word });
 });
 
 
-
-
+app.route("/name").get((req, res) => {
+  let { first: firstName, last: lastName } = req.query;
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+}).post();
 
 
 
