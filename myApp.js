4d2c990f3d53a4fe9134a1d1f01c96cddc30093a
Alex <ascott.sco@gmail.com>
2023-09-04 21:32:07 +0100

    finished

--- myApp.js
+++ myApp.js
@@ -1,31 +1,19 @@
+const bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 require('dotenv').config();
+require('body-parser');
 //console.log("Hello World");
 
 const path = __dirname + "/views/index.html";
+const mware = bodyParser.urlencoded({ extended: false });
 
 app.use("/public", express.static(__dirname + "/public"));
+app.use(mware);
 
-app.get("/", function(res) {
+app.get("/", (req, res) => {
   res.sendFile(path);
 });
-
-//let obj = {
-//  message: "Hello json",
-//};
-
-//app.get("/json", (req, res) => {
-//  if (process.env.MESSAGE_STYLE == "uppercase") {
-//    obj.message = obj.message.toUpperCase();
-//  }
-//  res.json(obj);
-//});
-
-//app.use("/", (req, res, next) => {
-//  console.log(`${req.method} ${req.path} - ${req.ip}`);
-//  next();
-//});
 
 app.get("/now", (req, next) => {
   req.time = new Date().toString();
@@ -44,27 +32,25 @@ app.route("/name").get((req, res) => {
   res.json({
     name: `${firstName} ${lastName}`
   });
-}).post();
+}).post((req, res) => {
+  let input = req.body.first + " " + req.body.last;
+  res.json({ name: input });
+});
 
+//let obj = {
+//  message: "Hello json",
+//};
 
+//app.get("/json", (req, res) => {
+//  if (process.env.MESSAGE_STYLE == "uppercase") {
+//    obj.message = obj.message.toUpperCase();
+//  }
+//  res.json(obj);
+//});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//app.use("/", (req, res, next) => {
+//  console.log(`${req.method} ${req.path} - ${req.ip}`);
+//  next();
+//});
 
 module.exports = app;
